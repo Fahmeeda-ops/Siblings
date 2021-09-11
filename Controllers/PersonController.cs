5b7fe2ea68e0285a87c7f162ed756c4d94f90bae
fahme <fahme@DESKTOP-E65R56G>
2021-09-11 18:43:57 -0400

    Add project files.

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Message()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult ShowPerson()
+        {
+            List<Person> persons = new List<Person>()
+            {
+                    new Person { firstName = "Fahmeeda", lastName = "Nazneen" },
+                    new Person { firstName = "Zakia", lastName = "Farheen" },
+                    new Person { firstName = "Abdul Qayyum", lastName = "Hazari" },
+
+            };
+            return View(persons);
+        }
+
+    }
+}
